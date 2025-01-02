55edb0e5910644420bd5a85bc5023fc800860e09
rmarticedeno <rmarticedeno@gmail.com>
2025-01-02 10:19:50 -0500

    use docker compose instead of .sh scripst

--- infraestructure/db.sh
+++
@@ -1,8 +0,0 @@
-docker run -d \
--p $POSTGRES_PORT:5432 \
--e POSTGRES_PASSWORD=$POSTGRES_PASSWORD \
--e PGDATA=/var/lib/postgresql/data/pgdata \
--e POSTGRES_USER=$POSTGRES_USER \
--v $POSTGRES_DATA:/var/lib/postgresql/data \
---name $POSTGRES_DOCKER_NAME \
---restart=always postgres:alpine
