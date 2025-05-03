69c6b3b0a340a251a0983bc28def48ee84942e62
rmarticedeno <rmarticedeno@gmail.com>
2025-05-03 10:41:34 -0400

    Improved Update for db side

--- src/format_message.py
+++ src/format_message.py
@@ -8,24 +8,31 @@ def db_upload(msg):
 
     connstring = os.getenv('DB_CONNSTRING')
     table = os.getenv('DB_Table')
-
+    
     try:
         msg_decoded = decodeMessage(msg)      # decodifico el mensaje utilizando la biblioteca synop del pymetdecoder,
 
+        time = msg_decoded['obs_time']
+        station_id = msg_decoded['station_id']
+
         columns = ', '.join(msg_decoded.keys())             # nombre de los campos de la tabla Test de la BD
         placeholders = ', '.join(['?'] * len(msg_decoded))
+
+        #print(msg_decoded, columns, placeholders)
         try:
-            conn = pyodbc.connect(connstring)
-            cursor = conn.cursor()
-            sql = f'INSERT INTO {table} ({columns}) VALUES ({placeholders})'   # consulta sql para insertar los valores decodificados en la tabla Test
-            cursor.execute(sql, tuple(msg_decoded.values()))
-            conn.commit()
+            with pyodbc.connect(connstring) as conn:
+                with conn.cursor() as cursor:
+                    basequeryUpdate = ', '.join([f'{x} = ?' for x in  msg_decoded.keys()])  
+                    queryUpdate =  f'UPDATE {table} SET {basequeryUpdate}'
+
+                    queryAdd =  f'INSERT INTO {table} ({columns}) VALUES ({placeholders})'
+                    querySelect = f"SELECT * FROM {table} WHERE obs_time = '{time}' AND station_id = '{station_id}'"
+                    rows = cursor.execute(querySelect).fetchall()
+                    cursor.execute(queryUpdate if len(rows) > 0 else queryAdd, tuple(msg_decoded.values()))
+                    conn.commit()
+
         except Exception as e:
-            logger.error(f"Error: {e}")
-        finally:
-            # Close the connection
-            cursor.close()
-            conn.close()
+            logger.error(f"Error asd: {e}")
 
     except Exception as e:
-        logging.error(f"Error: {e}")
+        logger.error(f"Error adsasd: {e}")
